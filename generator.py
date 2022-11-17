beed82f825181a4f517d132e09b3be195d8d50d3
Event 42 <event@c3r4s4.42madrid.com>
2022-11-17 01:21:33 +0100

    generador ok

---
+++ generator.py
@@ -0,0 +1,24 @@
+import ramdom
+def genrator():
+    al = random.randint(1,2)
+    final = "";
+    final = final.join(sustantivo())
+    final = final.join(espacio())
+    if al == 1)
+        final = final.join(verbo())
+    else:
+        final = final.join(adjetivo())
+    al = random.randint(1,2);
+    final = final.join(adjetivo())
+    final = final.join(espacio)
+    final = final.join(conecto())
+    final = final.join(espacio)
+    if al == 1:
+        final = final.join(adjetivo())
+    else
+        final = final.join(sustantivo());
+    return (final)
+
+number = random.randint(5,20)
+for range(1,number):
+    print(generator())
